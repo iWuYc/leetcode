0a7591c5b166f6c6f657b689df16c5a6b07a1ed7
Neil <iwuyc@foxmail.com>
2020-06-03 23:27:18 +0800

    日常提交：P007

---
+++ leetcode-part1/src/main/java/com/iwuyc/leetcode/p001_p100/P007.java
@@ -0,0 +1,30 @@
+package com.iwuyc.leetcode.p001_p100;
+
+import java.util.Stack;
+
+public interface P007 {
+    class Solution {
+        public int reverse(int x) {
+            if (!(x < -9 || x > 9)) {
+                return x;
+            }
+            boolean lassThenZero;
+            if (x < 0) {
+                lassThenZero = true;
+                x = -x;
+            } else {
+                lassThenZero = false;
+            }
+            int result = 0;
+            while (x != 0) {
+                result = result * 10 + x % 10;
+                x /= 10;
+                if (result < 0) {
+                    return 0;
+                }
+            }
+
+            return lassThenZero ? -result : result;
+        }
+    }
+}
---
+++ leetcode-part1/src/test/java/com/iwuyc/leetcode/p001_p100/P007Test.java
@@ -0,0 +1,14 @@
+package com.iwuyc.leetcode.p001_p100;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class P007Test {
+    @Test
+    public void name() {
+        P007.Solution solution = new P007.Solution();
+        int result = solution.reverse(-123);
+        System.out.println(result);
+    }
+}
