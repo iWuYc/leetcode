839b0e8105609b2db4ef1ea685e65470cd5217b2
Neil <iwuyc@foxmail.com>
2020-06-16 23:11:11 +0800

    日常提交：待完成问题P198

---
+++ leetcode-part1/src/main/java/com/iwuyc/leetcode/p001_p200/P198.java
@@ -0,0 +1,12 @@
+package com.iwuyc.leetcode.p001_p200;
+
+/**
+ * TODO
+ */
+public interface P198 {
+    class Solution {
+        public int rob(int[] nums) {
+            return 0;
+        }
+    }
+}
